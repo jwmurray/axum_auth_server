e6b7140c7ddd18bdb6f8fcafae8532cf1a98eba2
jwmu <jwmu@amazon.com>
2025-07-26 14:06:39 -0600

    finish sprint 1

--- auth-service/tests/api/signup.rs
+++ auth-service/tests/api/signup.rs
@@ -37,3 +37,26 @@ async fn should_return_422_if_malformed_input() {
         );
     }
 }
+
+#[tokio::test]
+async fn should_return_200_if_good_input() {
+    let app = TestApp::new().await;
+
+    let random_email = get_random_email();
+
+    let test_cases = [serde_json::json!({
+        "email": get_random_email(),
+        "password": "password123",
+        "requires2FA": true
+    })];
+
+    for test_case in test_cases.iter() {
+        let response = app.post_signup(test_case).await;
+        assert_eq!(
+            response.status().as_u16(),
+            200,
+            "Failed for input: {:?}",
+            test_case
+        );
+    }
+}
