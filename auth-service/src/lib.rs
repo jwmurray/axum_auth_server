62d13e33a0f56f9228f2fdc6eea8c44ec4b89771
jwmu <jwmu@amazon.com>
2025-08-23 05:51:25 -0600

    postgres database added

--- auth-service/src/lib.rs
+++ auth-service/src/lib.rs
@@ -1,5 +1,7 @@
 #![allow(unused_imports)]
 use std::error::Error;
+use sqlx::PgPool;
+use sqlx::postgres::PgPoolOptions;
 
 use axum::{
     http::StatusCode,
@@ -95,4 +97,9 @@ impl IntoResponse for AuthAPIError {
 
         (status, body).into_response()
     }
+}
+
+pub async fn get_postgres_pool(url: &str) -> Result<PgPool, sqlx::Error> {
+    // Create a new PostgreSQL connection pool
+    PgPoolOptions::new().max_connections(5).connect(url).await
 }
